09d2daf86032469bda4aab86de030292ed142452
lollipop190 <191250190@smail.nju.edu.cn>
2021-06-12 00:48:53 +0800

    perfect version on chrome

---
+++ src/views/statistics.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="statistics-container">
+      <div @click="changeCharts(0)"
+           :class="{'statistics-router':true, 'active': index===0}">
+        周
+      </div>
+      <div @click="changeCharts(1)"
+           :class="{'statistics-router':true, 'active': index===1}">
+        月
+      </div>
+      <div @click="changeCharts(2)"
+           :class="{'statistics-router':true, 'active': index===2}">
+        年
+      </div>
+    </div>
+    <template-charts ref="templateCharts"></template-charts>
+    <!-- <week v-if="index===0" ref="week"></week>
+    <month v-if="index===1" ref="month"></month>
+    <year v-if="index===2" ref="year"></year> -->
+  </div>
+</template>
+
+<script>
+import Week from "../components/statistics/week";
+import Month from "../components/statistics/month";
+import Year from "../components/statistics/year";
+import {getRes} from "../util/axiosAPI";
+import TemplateCharts from '../components/statistics/templateCharts.vue';
+
+export default {
+  name: "statistics",
+  components: {Year, Month, Week, TemplateCharts},
+  data() {
+    return {
+      index: 0,
+      list: [],
+    }
+  },
+  mounted() {
+    const _this = this;
+    getRes(
+        '/bill/allBills',
+        res => {
+          _this.list = res.data;
+          _this.$nextTick(() => {
+            this.$refs.templateCharts.drawWeek(this.list);
+          })
+        }
+    )
+  },
+  methods: {
+    changeCharts(index) {
+      if (index !== this.index) {
+        switch (index) {
+          case 0:
+            this.index = 0;
+            this.$nextTick(() => {
+              this.$refs.templateCharts.drawWeek(this.list);
+            })
+            break;
+          case 1:
+            this.index = 1;
+            this.$nextTick(() => {
+              this.$refs.templateCharts.drawMonth(this.list);
+            })
+            break;
+          case 2:
+            this.index = 2;
+            this.$nextTick(() => {
+              this.$refs.templateCharts.drawYear(this.list);
+            })
+            break;
+        }
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+.statistics-router {
+  margin: 10px 1px;
+  flex: 1;
+  text-align: center;
+  color: black;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+  text-decoration: none;
+}
+
+.statistics-container {
+  display: flex;
+}
+
+.active {
+  /*background-color: #25e09f*/
+  background-color: #fff585;
+}
+</style>
